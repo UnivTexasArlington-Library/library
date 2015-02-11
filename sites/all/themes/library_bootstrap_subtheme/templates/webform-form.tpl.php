4cef1aac97a0b2863e34b56e9b234ce012a9fbe6
kschenk <kschenk@uta.edu>
2015-02-11 11:41:03 -0600

    Webform templates
    added copies to theme

--- sites/all/themes/library_bootstrap_subtheme/templates/webform-form.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/webform-form.tpl.php
@@ -1,40 +1,41 @@
 <?php
-    /**
-    * @file
-    * Customize the display of a complete webform.
-    *
-    * This file may be renamed "webform-form-[nid].tpl.php" to target a specific
-    * webform on your site. Or you can leave it "webform-form.tpl.php" to affect
-    * all webforms on your site.
-    *
-    * Available variables:
-    * - $form: The complete form array.
-    * - $nid: The node ID of the Webform.
-    *
-    * The $form array contains two main pieces:
-    * - $form['submitted']: The main content of the user-created form.
-    * - $form['details']: Internal information stored by Webform.
-    */
-    // Print out the main part of the form.
-    // Feel free to break this up and move the pieces within the array.
 
-    // Always print out the entire $form. This renders the remaining pieces of the
-    // form that haven't yet been rendered above.
+/**
+ * @file
+ * Customize the display of a complete webform.
+ *
+ * This file may be renamed "webform-form-[nid].tpl.php" to target a specific
+ * webform on your site. Or you can leave it "webform-form.tpl.php" to affect
+ * all webforms on your site.
+ *
+ * Available variables:
+ * - $form: The complete form array.
+ * - $nid: The node ID of the Webform.
+ *
+ * The $form array contains two main pieces:
+ * - $form['submitted']: The main content of the user-created form.
+ * - $form['details']: Internal information stored by Webform.
+ *
+ * If a preview is enabled, these keys will be available on the preview page:
+ * - $form['preview_message']: The preview message renderable.
+ * - $form['preview']: A renderable representing the entire submission preview.
+ */
+?>
+<?php
+  // Print out the progress bar at the top of the page
+  print drupal_render($form['progressbar']);
 
-    foreach($form['submitted'] as $key=>$value){
-    if(substr($key, 0, 1)<>'#'){
-    $prefix = '<div class="form-group col-sm-12">';
-    $prefix .= '<label for="edit-submitted-'.$key.'" class="col-sm-6 control-label text-right">'.$value['#title'].'</label>';
-    $prefix .= '<div class="col-sm-6 control-label">';
+  // Print out the preview message if on the preview page.
+  if (isset($form['preview_message'])) {
+    print '<div class="messages warning">';
+    print drupal_render($form['preview_message']);
+    print '</div>';
+  }
 
-    $suffix = '</div></div>';
+  // Print out the main part of the form.
+  // Feel free to break this up and move the pieces within the array.
+  print drupal_render($form['submitted']);
 
-    $form['submitted'][$key]['#prefix'] = $prefix;
-    $form['submitted'][$key]['#suffix'] = $suffix;
-    unset($form['submitted'][$key]['#title']);
-    }
-
-    }
-
-    print drupal_render($form['submitted']);
-    print drupal_render_children($form);
+  // Always print out the entire $form. This renders the remaining pieces of the
+  // form that haven't yet been rendered above (buttons, hidden elements, etc).
+  print drupal_render_children($form);
