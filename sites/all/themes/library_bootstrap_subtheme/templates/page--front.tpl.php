b929ad1305d4682b1e3b2e0e1facea80befbdbaa
kschenk <kschenk@uta.edu>
2015-11-02 14:00:06 -0600

    Revert "remove extra div on front page template?"
    This reverts commit 596d21772f6b8eaaed507a9c1e6116dc11b24e57.

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -75,28 +75,32 @@
 ?>
 
 <div class="page-container container-fluid">
-    <?php if (!empty($page['lib_header'])): ?>
-  <div  class="row">
-    <?php if (!empty($page['feedback'])): ?>
-    <div class="col-md-2 col-sm-3 hidden-xs">
-      <?php print render($page['feedback']); ?>
-    </div> <!-- end feedback -->
-    <div class="col-md-8 col-sm-9 col-xs-12">
-        <?php else: ?>
-      <div class="col-md-8 col-sm-9 col-xs-12 col-sm-offset-3 col-md-offset-2">
+          <?php if (!empty($page['lib_header'])): ?>
+        <div  class="row">
+          <?php if (!empty($page['feedback'])): ?>
+          <div class="col-md-2 col-sm-3 hidden-xs">
+            <?php print render($page['feedback']); ?>
+          </div> <!-- end feedback -->
+          <div class="col-md-8 col-sm-9 col-xs-12 uta_header">
+
+              <?php else: ?>
+          <div class="col-md-8 col-sm-9 col-xs-12 uta_header col-sm-offset-3 col-md-offset-2">
+            <?php endif; ?>
+          
+            <div id="libhrs" class="row">
+              <div class="col-md-7 col-sm-7 col-xs-12"><?php print render($page['lib_header']); ?></div>
+                <?php if (!empty($page['todays_hrs'])): ?>
+              <div class="col-md-5 col-sm-5 col-xs-12"><?php print render($page['todays_hrs']); ?></div>
+            </div> <!-- end #libhrs row -->
+              <?php endif; ?>
+    
+        </div> <!-- end uta_header -->
+
         <?php endif; ?>
-      
-        <div id="libhrs" class="row">
-          <div class="col-md-7 col-sm-7 col-xs-12"><?php print render($page['lib_header']); ?></div>
-            <?php if (!empty($page['todays_hrs'])): ?>
-          <div class="col-md-5 col-sm-5 col-xs-12"><?php print render($page['todays_hrs']); ?></div>
-        </div> <!-- end #libhrs row -->
-          <?php endif; ?>
-  </div> <!-- end uta_header -->
 
-  <?php endif; ?>
+      </div> <!-- end row -->
 
-<!--</div>  end row -->
+
 
       <?php if (!empty($page['modal'])): ?>
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
