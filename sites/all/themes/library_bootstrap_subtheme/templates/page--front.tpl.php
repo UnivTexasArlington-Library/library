db0ed54800c10123487b69818f90f6c4cf112555
kschenk <kschenk@uta.edu>
2015-11-09 13:02:56 -0600

    add php end if statement to front template

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -94,22 +94,6 @@
         <span class="icon-bar"></span>
       </button>
     </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
   </div>
 </header>
 
@@ -126,8 +110,9 @@
     <?php endif; ?>
 
     <section<?php print $content_column_class; ?> class="center-container">
-      <?php if (!empty($page['highlighted'])): ?>
+      <?php if (!empty($page['lib_header'])): ?>
         <div id="lib_header"><?php print render($page['lib_header']); ?></div>
+      <?php endif; ?>
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
