b591868932a36455502a6ee5b3c72defc56c5554
kschenk <kschenk@uta.edu>
2015-11-13 14:21:17 -0600

    technology icon on location

--- sites/all/themes/library_bootstrap_subtheme/templates/field--field_technology_url.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/field--field_technology_url.tpl.php
@@ -51,7 +51,7 @@
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><span class="glyphicon"><img src="/sites/default/files/default-images/technology.png"></span> <?php print render($item); ?></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><span class="glyphicon"><img src="/sites/default/files/default_images/technology.png"></span> <?php print render($item); ?></div>
     <?php endforeach; ?>
   </div>
 </div>
