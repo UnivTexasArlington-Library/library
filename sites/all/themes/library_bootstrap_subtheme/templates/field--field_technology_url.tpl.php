858a4833105480fa7b55b510a84e1f288550e594
kschenk <kschenk@uta.edu>
2015-11-13 13:19:12 -0600

    field and region templates

--- sites/all/themes/library_bootstrap_subtheme/templates/field--field_technology_url.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/field--field_technology_url.tpl.php
@@ -51,7 +51,7 @@
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><span class="glyphicon"><img src="http://library.uta.edu/sites/default/files/technology.png"></span> <?php print render($item); ?></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><span class="glyphicon orange"><img src="http://library.uta.edu/sites/default/files/technology.png"></span> <?php print render($item); ?></div>
     <?php endforeach; ?>
   </div>
 </div>
