9ef68ad3da45147c9e3dbb049fa7cb4eb1f37c0d
kschenk <kschenk@uta.edu>
2015-11-13 07:42:29 -0600

    Remove Jumbotron class from templates

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -114,7 +114,7 @@
         <div id="lib_header"><?php print render($page['lib_header']); ?></div>
       <?php endif; ?>
       <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+        <div class="highlighted"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
