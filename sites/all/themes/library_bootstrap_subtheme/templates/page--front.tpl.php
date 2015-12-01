b6884126fce6ef3fa0d8674259385f885270c78e
kschenk <kschenk@uta.edu>
2015-12-01 10:19:48 -0600

    add todays hours in "hours" modal

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -119,9 +119,9 @@
           <div class="col-md-9 col-sm-9 col-xs-12"><?php print render($page['lib_header']); ?></div>
           <?php if (!empty($page['modal'])): ?>
           <div class="col-md-3 col-sm-3 col-xs-12"><?php print render($page['modal']); ?></div>
-          <?php endif; ?>
+
         </div>
-      <?php endif; ?>
+
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
@@ -139,6 +139,26 @@
           </div>
         </div>
       </div> <!-- end.navbar -->
+
+<?php if (!empty($page['modal'])): ?>
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+              </div>
+              <div class="modal-body">
+                <?php print render($page['todays_hrs']); ?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php endif; ?>
+
+
      <!-- <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
         <h1 class="page-header"><?php print $title; ?></h1>
