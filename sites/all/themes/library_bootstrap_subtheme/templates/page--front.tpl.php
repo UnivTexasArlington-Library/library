98a0ee7aece8c91b366d06982f3cc041c928fe45
kschenk <kschenk@uta.edu>
2015-12-01 10:41:46 -0600

    return front page template to a working version

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -140,25 +140,6 @@
         </div>
       </div> <!-- end.navbar -->
 
-<?php if (!empty($page['modal'])): ?>
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-              </div>
-              <div class="modal-body">
-                <?php print render($page['todays_hrs']); ?>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      <?php endif; ?>
-
-
      <!-- <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
         <h1 class="page-header"><?php print $title; ?></h1>
