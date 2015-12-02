2629c8dcfd59c8f8756b0dd8222efe6c3ce235da
kschenk <kschenk@uta.edu>
2015-12-02 14:49:26 -0600

    today's hours & contact us region & css changes

--- sites/all/themes/library_bootstrap_subtheme/templates/region--feedback.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/region--feedback.tpl.php
@@ -33,10 +33,10 @@
 	<div class="modal" id="menuContact" tabindex="-1" role="dialog" aria-labelledby="Contact Us">
 	  <div class="modal-dialog" role="document">
 	    <div class="modal-content">
-	      <div class="modal-header">
+	    	<div class="modal-header"> 
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	       <!-- <h4 class="modal-title">We Listen!</h4> -->
-	      </div>
+	       	<h4 class="modal-title">We Listen!</h4>
+	      </div> 
 	      <div class="modal-body">
 	      	<?php print $content; ?>
 	      </div>
