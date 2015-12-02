e968d9ab9a66d85ddf9794d6135bfffeababdea8
kschenk <kschenk@uta.edu>
2015-12-02 14:03:12 -0600

    add today's hours

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -119,6 +119,7 @@
           <div class="col-md-9 col-sm-9 col-xs-12"><?php print render($page['lib_header']); ?></div>
           <?php if (!empty($page['modal'])): ?>
           <div class="col-md-3 col-sm-3 col-xs-12"><?php print render($page['modal']); ?></div>
+          <?php print render($page['todays_hrs']); ?>
           <?php endif; ?>
         </div>
       <?php endif; ?>
