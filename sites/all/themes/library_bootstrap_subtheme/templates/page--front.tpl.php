c4a9576e10344f2e6c26e30c51068624ee02a38f
kschenk <kschenk@uta.edu>
2015-12-03 16:28:47 -0600

    searchbox & more

--- sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/page--front.tpl.php
@@ -102,13 +102,15 @@
 
 
   <div class="row">
-
+    <div class="row row-offcanvas row-offcanvas-left">
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-xs-6 col-sm-3 col-md-2 sidebar-offcanvas" role="navigation" id="sidebar">4
+      <aside class="col-xs-6 col-sm-3 col-md-2 sidebar-offcanvas" role="navigation" id="sidebar">
+        
         <?php print render($page['sidebar_first']); ?>
+
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
-
+    </div>
     <section class="center-container col-md-8 col-sm-9 col-xs-12">
       
       <?php if (!empty($page['notification'])): ?>
