2c6cfe84f85ed5050a09d836651dfa5a0a998e25
Krystal Schenk <kschenk@uta.edu>
2016-08-08 10:01:50 -0500

    Event Reservation Menu

--- sites/all/themes/library_bootstrap_subtheme/template.php
+++ sites/all/themes/library_bootstrap_subtheme/template.php
@@ -69,6 +69,11 @@ function bootstrap_menu_tree__menu_gis(&$variables) {
   return '<ul class="menu nav nav-tabs">' . $variables['tree'] . '</ul>';
 }
 
+//adding class to Events Reservation menu
+function bootstrap_menu_tree__menu_event_reservation(&$variables) {
+  return '<ul class="menu nav nav-tabs">' . $variables['tree'] . '</ul>';
+}
+
 //adding class to friends (on production) menu
 function bootstrap_menu_tree__menu_friends_library(&$variables) {
   return '<ul class="menu nav nav-tabs">' . $variables['tree'] . '</ul>';
