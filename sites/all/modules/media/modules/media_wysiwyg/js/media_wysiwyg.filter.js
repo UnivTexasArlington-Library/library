6edf5cd3ec262875b425034b056d0a3fe1390b0a
Krystal Schenk <kschenk@uta.edu>
2016-11-17 08:43:41 -0600

    Media module update

--- sites/all/modules/media/modules/media_wysiwyg/js/media_wysiwyg.filter.js
+++ sites/all/modules/media/modules/media_wysiwyg/js/media_wysiwyg.filter.js
@@ -43,10 +43,17 @@
           // Re-build the media if the macro has changed from the tagmap.
           if (!media && media_definition.fid) {
             Drupal.media.filter.ensureSourceMap();
-            var source = Drupal.settings.mediaSourceMap[media_definition.fid];
-            media = document.createElement(source.tagName);
-            media.src = source.src;
-            media.innerHTML = source.innerHTML;
+            var source;
+            if (source = Drupal.settings.mediaSourceMap[media_definition.fid]) {
+              media = document.createElement(source.tagName);
+              media.src = source.src;
+              media.innerHTML = source.innerHTML;
+            }
+            else {
+              // If the media element can't be found, leave it in to be resolved
+              // by the user later.
+              continue;
+            }
           }
 
           // Apply attributes.
@@ -182,7 +189,7 @@
       // media_get_file_without_label().
       //
       // Finds the media-element class.
-      var classRegex = 'class=([\'"])[^\\1]*?media-element';
+      var classRegex = 'class=[\'"][^\'"]*?media-element';
       // Image tag with the media-element class.
       var regex = '<img[^>]+' + classRegex + '[^>]*?>';
       // Or a span with the media-element class (used for documents).
