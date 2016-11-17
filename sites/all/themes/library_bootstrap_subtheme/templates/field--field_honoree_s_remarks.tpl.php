f3f2ccfc15366f65742e8972d48290dffba27d7e
Krystal Schenk <kschenk@uta.edu>
2016-11-17 11:32:31 -0600

    field template fix #2

--- sites/all/themes/library_bootstrap_subtheme/templates/field--field_honoree_s_remarks.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/field--field_honoree_s_remarks.tpl.php
@@ -51,7 +51,11 @@
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?><blockquote>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></blockquote></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+      	<blockquote>
+      		<?php print render($item); ?>
+      	</blockquote>
+      </div>
     <?php endforeach; ?>
   </div>
 </div>
