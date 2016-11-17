8774e4fb023634971e4fd790f3a615768aa8f203
Krystal Schenk <kschenk@uta.edu>
2016-11-17 11:29:44 -0600

    fix honoree field template

--- sites/all/themes/library_bootstrap_subtheme/templates/field--field_honoree_s_remarks.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/field--field_honoree_s_remarks.tpl.php
@@ -51,7 +51,7 @@
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<blockquote><?php print $item_attributes[$delta]; ?>><?php print render($item); ?></blockquote></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?><blockquote>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></blockquote></div>
     <?php endforeach; ?>
   </div>
 </div>
