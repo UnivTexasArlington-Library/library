4c53e45443eddd8219523fff028db526a7c0d5d0
Krystal Schenk <kschenk@uta.edu>
2016-11-17 15:45:47 -0600

    add emphsis to honoree remarks

--- sites/all/themes/library_bootstrap_subtheme/templates/field--field_honoree_s_remarks.tpl.php
+++ sites/all/themes/library_bootstrap_subtheme/templates/field--field_honoree_s_remarks.tpl.php
@@ -52,9 +52,9 @@
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-      	<blockquote>
+      	<blockquote><em>
       		<?php print render($item); ?>
-      	</blockquote>
+      	</em></blockquote>
       </div>
     <?php endforeach; ?>
   </div>
