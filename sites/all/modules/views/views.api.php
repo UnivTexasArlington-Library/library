e91c815409e37083412da4644b53529e56330d76
Krystal Schenk <kschenk@uta.edu>
2018-04-16 10:28:22 -0500

    views module updat

--- sites/all/modules/views/views.api.php
+++ sites/all/modules/views/views.api.php
@@ -981,7 +981,7 @@ function hook_views_form_substitutions() {
  * @param array $args
  *   An array of arguments passed into the view.
  */
-function hook_views_pre_preview(&$view, &$display_id, &$args) {
+function hook_views_pre_view(&$view, &$display_id, &$args) {
   // Change the display if the acting user has 'administer site configuration'
   // permission, to display something radically different.
   // (Note that this is not necessarily the best way to solve that task. Feel
@@ -1260,9 +1260,12 @@ function hook_views_ajax_data_alter(&$commands, $view) {
  * This hook should fire whenever a view is enabled, disabled, created,
  * updated, or deleted.
  *
+ * @param string $cid
+ *   The cache identifier that is being cleared.
+ *
  * @see views_invalidate_cache()
  */
-function hook_views_invalidate_cache() {
+function hook_views_invalidate_cache($cid) {
   cache_clear_all('views:*', 'cache_mymodule', TRUE);
 }
 
