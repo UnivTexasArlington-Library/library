297573a4f5f3376edaf1e7aeeb26322d99a3d61b
Krystal Schenk <kschenk@uta.edu>
2018-08-08 09:19:20 -0500

    module updates

--- sites/all/modules/office_hours/office_hours.js
+++ sites/all/modules/office_hours/office_hours.js
@@ -1,29 +1,54 @@
 (function ($) {
 
   Drupal.behaviors.office_hours = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
 
       // Hide every item above the max blocks per day.
       $(".oh-hide", context).parent().hide();
 
       $(".oh-add-more-link", context).each(function (i) {
-        $(this).parent().children("div.office-hours-block").hide();
-        // If the previous row has an "Add more hours" link, and the office-hours-block is hidden, don't show this line.
-        $this_tr = $(this).closest("tr");
-        if ($this_tr.prev().find(".oh-add-more-link").length && !$this_tr.prev().find("div.office-hours-block:visible").length) {
-          $this_tr.hide();
-        }
-      })
-      .bind('click', show_upon_click);
+          $(this).parent().children("div.office-hours-block").hide();
+          // If the previous row has an "Add more hours" link, and the office-hours-block is hidden, don't show this line.
+          $this_tr = $(this).closest("tr");
+          if ($this_tr.prev().find(".oh-add-more-link").length && !$this_tr.prev().find("div.office-hours-block:visible").length) {
+            $this_tr.hide();
+          }
+        })
+        .bind('click', show_upon_click);
 
       fix_striping();
 
       // Clear the content of this block, when user clicks "Clear/Remove".
-      $('.oh-clear-link').bind('click', function(e) {
-        $(this).parent().parent().find('.form-select').each(function() {
+      $('.oh-clear-link').bind('click', function (e) {
+        $(this).parent().parent().find('.form-select').each(function () {
           $(this).val($("#target option:first").val());
         });
         e.preventDefault();
+      });
+
+      // Copy values from previous day, when user clicks "Same as above".
+      $('.oh-same-link').bind('click', function (e) {
+        e.preventDefault();
+        var current_day = parseInt($(this).parent().parent().attr('data-day'));
+        var previous_day = current_day - 1;
+        if (current_day == 0) {
+          previous_day = current_day + 6;
+        }
+
+        // Select current table.
+        var tbody = $(this).closest('tbody');
+        // Div's from current day.
+        var current_selector = tbody.find('div[data-day="' + current_day + '"]');
+        // Div's from previous day.
+        var previous_selector = tbody.find('div[data-day="' + previous_day + '"]');
+
+        // Replace current day values with the ones from the previous day.
+        previous_selector.find('.form-select:visible').each(function (index, value) {
+          var previous_value = $(this).val();
+          current_selector.find('.form-select').eq(index).val(previous_value);
+          //"unhide" copied value using add more link
+          current_selector.find('.form-select').eq(index).closest('tr').find('.oh-add-more-link').click();
+        });
       });
 
       // Show an office-hours-block, when user clicks "Add more".
